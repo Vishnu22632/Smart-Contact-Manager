3bde2066378b106162d74e5cd41dc2645f3afce2
Vishnu <vishnu22632@gmail.com>
2026-01-18 09:03:31 +0545

    Added fragment , navbar and apply dark them

--- src/main/java/com/scm/controller/HomeController.java
+++ src/main/java/com/scm/controller/HomeController.java
@@ -8,11 +8,42 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     @RequestMapping("/home")
-    public String home(Model model){
+    public String home(Model model) {
         model.addAttribute("name", "Vishnu Thakur");
         model.addAttribute("github", "https://github.com/Vishnu22632/Smart-Contact-Manager");
         return "home";
     }
+
+    @RequestMapping("/services")
+    public String servicesPage() {
+        return "services";
+    }
+
+    @RequestMapping("/about")
+    public String aboutPage() {
+        return "about";
+    }
+
+    @RequestMapping("/contact")
+    public String contactPage() {
+        return "contact";
+    }
+
+    @RequestMapping("/login")
+    public String logIn() {
+        return "login";
+    }
+
+    @RequestMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+
+
+
+
+
 
 
 }
